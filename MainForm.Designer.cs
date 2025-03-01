8aeef619a73bd95dec6277402952e3168e3c1d10
CesarFSN <fersana24@outlook.com>
2025-02-28 19:25:09 -0700

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Programación
+ * Fecha: 26/02/2025
+ * Hora: 01:19 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+namespace HolaMundo_SNCF
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button BtnAceptar;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.BtnAceptar = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Maroon;
+			this.label1.Location = new System.Drawing.Point(23, 43);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(149, 111);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Hola CETis No. 108";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// BtnAceptar
+			// 
+			this.BtnAceptar.Location = new System.Drawing.Point(45, 166);
+			this.BtnAceptar.Name = "BtnAceptar";
+			this.BtnAceptar.Size = new System.Drawing.Size(92, 43);
+			this.BtnAceptar.TabIndex = 1;
+			this.BtnAceptar.Text = "&Aceptar";
+			this.BtnAceptar.UseVisualStyleBackColor = true;
+			this.BtnAceptar.Click += new System.EventHandler(this.BtnAceptarClick);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(206, 33);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(187, 176);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.ClientSize = new System.Drawing.Size(432, 261);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.BtnAceptar);
+			this.Controls.Add(this.label1);
+			this.HelpButton = true;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "Mi Primer Programa";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Cesar Fernando
+ * Fecha: 26/02/2025
+ * Hora: 01:19 p. m.
+ * Programa: HolaMundo
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HolaMundo_SNCF
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Label1Click(object sender, EventArgs e)
+		{
+	
+		}
+		void BtnAceptarClick(object sender, EventArgs e)
+		{
+			DialogResult opcion;
+			opcion=MessageBox.Show("¿Realmente Desea Salir?", "Aviso",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+			
+			if(opcion==DialogResult.Yes)
+			{
+				this.Close();
+			}
+			
+		}
+		void PictureBox1Click(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
